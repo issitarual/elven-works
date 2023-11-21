940cef12096d653bd173c8a7b8f3b1d2ef997c35
Laura Rezende <laura.tracierra@gmail.com>
2023-11-21 18:33:19 -0300

    fix: setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+
     sudo apt update && sudo apt install ansible curl git nano unzip -y
     cd /tmp
-
-    git clone https://github.com/64J0/bootcamp-sre-elvenworks
-
+    git clone https://github.com/issitarual/elven-works.git
     # delete the default values
-    rm ./bootcamp-sre-elvenworks/ansible/wordpress-aws/roles/mysql-server/defaults/main.yml
-
-    tee -a ./bootcamp-sre-elvenworks/ansible/wordpress-aws/roles/mysql-server/defaults/main.yml << END
+    rm ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml
+    tee -a ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml << END
     ---
     rds_db_host: "${rds_db_host}"
     rds_db_port: "${rds_db_port}"
@@ -17,6 +15,6 @@
     wordpress_db_username: "${wordpress_db_username}"
     wordpress_db_password: "${wordpress_db_password}"
     END
-
-    cp ./bootcamp-sre-elvenworks/ansible/wordpress-aws/roles/mysql-server/defaults/main.yml ./bootcamp-sre-elvenworks/ansible/wordpress-aws/roles/wordpress/defaults/main.yml
-    sudo ansible-playbook ./bootcamp-sre-elvenworks/ansible/wordpress-aws/wordpress.yml
+    cp ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml ./elven-works/ansible/wordpress-challenge/roles/wordpress/defaults/main.yml
+    sudo ansible-playbook ./elven-works/ansible/wordpress-challenge/wordpress.yml
+    EOF
