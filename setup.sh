b07cef234fa1bb7b6055080a1bc3d1afcd556684
Laura Rezende <laura.tracierra@gmail.com>
2023-11-21 19:16:33 -0300

    fix: setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,20 +1,8 @@
-#!/bin/bash
-
-    sudo apt update && sudo apt install ansible curl git nano unzip -y
-    cd /tmp
+ #!/bin/bash 
+sudo apt update && sudo apt install curl ansible unzip -y 
+cd /tmp
     git clone https://github.com/issitarual/elven-works.git
-    # delete the default values
-    rm ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml
-    tee -a ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml << END
-    ---
-    rds_db_host: "${rds_db_host}"
-    rds_db_port: "${rds_db_port}"
-    rds_db_username: "${rds_db_username}"
-    rds_db_password: "${rds_db_password}"
-    wordpress_db_name: "${wordpress_db_name}"
-    wordpress_db_username: "${wordpress_db_username}"
-    wordpress_db_password: "${wordpress_db_password}"
-    END
-    cp ./elven-works/ansible/wordpress-challenge/roles/mysql/defaults/main.yml ./elven-works/ansible/wordpress-challenge/roles/wordpress/defaults/main.yml
-    sudo ansible-playbook ./elven-works/ansible/wordpress-challenge/wordpress.yml
-    EOF
+unzip ansible.zip
+cd ansible
+sudo ansible-playbook wordpress.yml --extra-vars "wp_db_name=${wordpress_db_name} wp_db_username=${wordpress_db_username} wp_db_password=${wordpress_db_password} wp_db_host=${rds_db_host} session_save_path=''" 
+
