9b642d8c4f4cd3df8caa8dfd5c8306522ded1d1f
Laura Rezende <laura.tracierra@gmail.com>
2023-11-21 19:25:35 -0300

    fix: add variables

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ cd /tmp
 git clone https://github.com/issitarual/elven-works.git
 unzip ansible.zip
 cd ansible
-sudo ansible-playbook wordpress.yml --extra-vars "wp_db_name=${wordpress_db_name} wp_db_username=${wordpress_db_username} wp_db_password=${wordpress_db_password} wp_db_host=${rds_db_host} session_save_path=''" 
-
+sudo ansible-playbook wordpress.yml --extra-vars "wp_db_name=${wordpress_db_name} wp_db_username=${wordpress_db_username} wp_db_password=${wordpress_db_password} wp_db_host=${rds_db_host} rds_db_host=${rds_db_host} rds_db_port=${rds_db_port} rds_db_username=${rds_db_username} rds_db_password=${rds_db_password} session_save_path=''" 
