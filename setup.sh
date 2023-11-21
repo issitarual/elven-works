6eed12e4365045663b11f166437d8af99d3e256c
Laura Rezende <laura.tracierra@gmail.com>
2023-11-21 19:21:51 -0300

    fix: change variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
- #!/bin/bash 
+#!/bin/bash 
 sudo apt update && sudo apt install curl ansible unzip -y 
 cd /tmp
-    git clone https://github.com/issitarual/elven-works.git
+git clone https://github.com/issitarual/elven-works.git
 unzip ansible.zip
 cd ansible
 sudo ansible-playbook wordpress.yml --extra-vars "wp_db_name=${wordpress_db_name} wp_db_username=${wordpress_db_username} wp_db_password=${wordpress_db_password} wp_db_host=${rds_db_host} session_save_path=''" 
